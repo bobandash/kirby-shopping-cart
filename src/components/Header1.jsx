2e02ffdb8dfe3c6dd7781462b1eea3769bc76db4
Bruce Hsu <brucehsu1126@gmail.com>
2023-08-28 19:29:21 -0700

    add header html  and styling

---
+++ src/components/Header1.jsx
@@ -0,0 +1,23 @@
+import KirbyLogo from '../assets/kirby-logo.png';
+import { Link } from 'react-router-dom';
+import styles from './Header1.module.css'
+
+function Header1(){
+  return (
+    <header className = {styles.header}>
+      <div className = {styles["container-header"]}>
+        <div><Link to="/"><img className = {styles["kirby-logo"]} src = {KirbyLogo} alt = "Kirby Logo"/></Link></div>
+        <form className = {styles["search-bar-form"]}>
+          <input type = "text" placeholder = "Search Products..."/>
+          <button type = "submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+        <div className = {styles["profile-cart-container"]}>
+          <Link to="/"><i className="fa-solid fa-user"></i><span>Sign In/Register</span></Link>
+          <Link to="/"><i className="fa-solid fa-cart-shopping"></i><span>Cart</span></Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header1;
