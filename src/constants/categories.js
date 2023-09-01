e1838045dcb39b03fa06889824976063913a0379
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-01 15:24:33 -0700

    add categories as enum to match fakestoreapi categories

---
+++ src/constants/categories.js
@@ -0,0 +1,9 @@
+// Contains enum with strings corresponding to category in database
+// TO-DO: change to appropriate categories once able to create database
+const CATEGORIES = {
+  Games: 'electronics',
+  Plushies: `men's clothing`,
+  Keychains: 'jewelry'
+}
+
+export default CATEGORIES;
