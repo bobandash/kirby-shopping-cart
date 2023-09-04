2d8cf8fed4fc9c1d9287ac214895d841b339c537
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-03 21:14:48 -0700

    add snapshot test to error page

---
+++ src/components/errorpage/__tests__/ErrorPage.test.jsx
@@ -0,0 +1,15 @@
+import {describe, it, expect} from 'vitest';
+import ErrorPage from '../ErrorPage';
+import {render} from "@testing-library/react";
+import { BrowserRouter as Router } from 'react-router-dom';
+
+describe('Error Page', () => {
+  it('renders correctly', () => {
+    const {container} = render(
+    <Router>
+      <ErrorPage />
+    </Router>
+    )
+    expect(container).toMatchSnapshot();
+  })
+})
