87c9c3336c9c0cc7bb13208f076ff1a9dd50cb4a
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-04 13:31:38 -0700

    add cart items state to router

--- src/router.jsx
+++ src/router.jsx
@@ -3,21 +3,43 @@ import App from './App.jsx'
 import ErrorPage from "./components/errorpage/ErrorPage.jsx"
 import ProductPage from "./components/product-page/ProductPage.jsx"
 import CategoryPage from "./components/category-page/Category.jsx"
+import { useState } from "react"
 
 const Router = () => {
+  const [cartItems, setCartItems] = useState([{quantity: 77}]);
+
+  function addCartItem(item, itemQuantity){
+    const hasItem = cartItems.filter(cartItem => cartItem.id === item.id).length > 0 ? true : false;
+
+    if(hasItem){
+      setCartItems(cartItems.map(cartItem => {
+        if(cartItem.id === item.id){
+          return cartItem.quantity += itemQuantity;
+        }
+        return cartItem;
+      }))
+    } else {
+      setCartItems(...cartItems, {...item, quantity: itemQuantity})
+    }
+  }
+
+  function removeCartItem(item){
+    setCartItems(cartItems.filter(cartItem => {cartItem.id !== item.id}));
+  }
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App />,
+      element: <App cartItems ={cartItems}/>,
       errorElement: <ErrorPage />
     },
     {
       path: "products/:name",
-      element: <ProductPage />
+      element: <ProductPage cartItems ={cartItems} />
     },
     {
       path: "category/:name",
-      element: <CategoryPage />
+      element: <CategoryPage cartItems ={cartItems} />
     }
   ])
 
