3c46a9ff28e6c85bf8daa2ccaaf7b3d3a9d7b873
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-11 22:01:01 -0700

    add initial header opacity when not scrolled

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -5,6 +5,7 @@ import SubHeader from './Subheader';
 import cartIcon from '../../assets/Cart/empty_cart.png';
 import kirbyCartIcon from '../../assets/Cart/kirby_in_cart.png';
 import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react';
 
 function Header1({cartItems}){
   const addQuantities = (a,b) => a + b;
@@ -16,9 +17,57 @@ function Header1({cartItems}){
     cartItemQuantity = cartItemQuantitiesArray.reduce(addQuantities)
   }
 
+  const [atTop, setAtTop] = useState(true);
+  const [y, setY] = useState(window.scrollY);
+
+  window.addEventListener("scroll", () => {
+    setY(window.scrollY);
+  })
+
+  useEffect(() => {
+    if(y === 0){
+      setAtTop(true);
+    } else {
+      setAtTop(false);
+    }
+  }, [y])
+
   // TO-DO: add search bar functionality
   function handleSearchClick(e) {
     e.preventDefault();
+  }
+
+  if(atTop){
+    return(
+      <header className = {`${styles.header} ${styles.opaque}`}>
+        <div className = {styles["container-header"]}>
+          {/* mobile hamburger bar */}
+          <div className = {styles["mobile-icon-container"]}><i className="fa-2x fa-solid fa-bars"></i></div>        
+          <div><Link to="/"><img className = {styles["kirby-logo"]} src = {KirbyLogo} alt = "Kirby Logo"/></Link></div>
+          <form onSubmit = {(e) => {handleSearchClick(e)}} className = {`${styles["search-bar-form"]} ${styles["desktop-icon-container"]}`}>
+            <input type = "text" placeholder = "Search Products..."/>
+            <button type = "submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+          </form>
+          <div className = {styles["profile-cart-container"]}>
+            <Link to="/" className = {styles["desktop-icon-container"]}><i className="fa-2x fa-solid fa-user"></i><span>Sign In/Register</span></Link>
+            {(cartItems.length === 0) ?
+              <Link to="/cart"><img className = {styles["cart-icon"]} src = {cartIcon}/><span>Cart</span></Link> :
+              <Link to="/cart">
+                <div className = {styles["cart-icon-container"]}>
+                  <img className = {styles["cart-icon"]} src = {kirbyCartIcon} />
+                  {cartItemQuantity > 99 ? 
+                    <p className = {`${styles["cart-quantity"]} ${styles["three-digits"]}`}>99+</p> :
+                    <p className = {`${styles["cart-quantity"]} ${styles["less-than-two-digits"]}`}>{cartItemQuantity}</p>
+                  }
+                </div>
+                <span>Cart</span>
+              </Link>
+            }
+          </div>
+        </div>
+        <SubHeader />
+      </header>   
+    )
   }
 
   return (
