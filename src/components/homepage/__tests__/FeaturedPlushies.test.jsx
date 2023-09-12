1cda7cf3db63fcd3fa591e7c8a39868844fa2977
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-12 16:11:48 -0700

    add other kirby characters to desktop landing screen

--- src/components/homepage/__tests__/FeaturedPlushies.test.jsx
+++ src/components/homepage/__tests__/FeaturedPlushies.test.jsx
@@ -1,5 +1,5 @@
 import FeaturedPlushies from "../FeaturedPlushies";
-import {render, screen} from "@testing-library/react";
+import {render} from "@testing-library/react";
 import { describe, it, expect } from 'vitest';
 
 describe('Featured Plushies Component', () => {
