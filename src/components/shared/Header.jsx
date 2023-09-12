5032076ebc5bdbeb2b026fa303793e01ff3baa6f
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-11 19:37:29 -0700

    fix responsive styling for header and subheader

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -4,6 +4,7 @@ import styles from './Header.module.css'
 import SubHeader from './Subheader';
 import cartIcon from '../../assets/Cart/empty_cart.png';
 import kirbyCartIcon from '../../assets/Cart/kirby_in_cart.png';
+import PropTypes from 'prop-types'
 
 function Header1({cartItems}){
   const addQuantities = (a,b) => a + b;
@@ -15,18 +16,25 @@ function Header1({cartItems}){
     cartItemQuantity = cartItemQuantitiesArray.reduce(addQuantities)
   }
 
+  // TO-DO: add search bar functionality
+  function handleSearchClick(e) {
+    e.preventDefault();
+  }
+
   return (
     <header className = {styles.header}>
       <div className = {styles["container-header"]}>
+        {/* mobile hamburger bar */}
+        <div className = {styles["mobile-icon-container"]}><i className="fa-2x fa-solid fa-bars"></i></div>        
         <div><Link to="/"><img className = {styles["kirby-logo"]} src = {KirbyLogo} alt = "Kirby Logo"/></Link></div>
-        <form className = {styles["search-bar-form"]}>
+        <form onSubmit = {(e) => {handleSearchClick(e)}} className = {`${styles["search-bar-form"]} ${styles["desktop-icon-container"]}`}>
           <input type = "text" placeholder = "Search Products..."/>
-          <button type = "submit"><i className="fa-lg fa-solid fa-magnifying-glass"></i></button>
+          <button type = "submit"><i className="fa-solid fa-magnifying-glass"></i></button>
         </form>
         <div className = {styles["profile-cart-container"]}>
-          <Link to="/"><i className="fa-lg fa-solid fa-user "></i><span>Sign In/Register</span></Link>
+          <Link to="/" className = {styles["desktop-icon-container"]}><i className="fa-2x fa-solid fa-user"></i><span>Sign In/Register</span></Link>
           {(cartItems.length === 0) ?
-            <Link to="/cart"><img className = {styles["cart-icon"]} src = {cartIcon} /><span>Cart</span></Link> :
+            <Link to="/cart"><img className = {styles["cart-icon"]} src = {cartIcon}/><span>Cart</span></Link> :
             <Link to="/cart">
               <div className = {styles["cart-icon-container"]}>
                 <img className = {styles["cart-icon"]} src = {kirbyCartIcon} />
@@ -43,6 +51,10 @@ function Header1({cartItems}){
       <SubHeader />
     </header>
   )
+}
+
+Header1.propTypes = {
+  cartItems: PropTypes.array
 }
 
 export default Header1;
