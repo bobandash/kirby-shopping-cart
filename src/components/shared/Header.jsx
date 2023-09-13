bcef96646628d7f4fb9cc2f658547c0be7e3ed8e
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-12 18:12:58 -0700

    minor refactor

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -37,41 +37,10 @@ function Header1({cartItems}){
     e.preventDefault();
   }
 
-  if(atTop){
-    return(
-      <header className = {`${styles.header} ${styles.opaque}`}>
-        <div className = {styles["container-header"]}>
-          {/* mobile hamburger bar */}
-          <div className = {styles["mobile-icon-container"]}><i className="fa-2x fa-solid fa-bars"></i></div>        
-          <div><Link to="/"><img className = {styles["kirby-logo"]} src = {KirbyLogo} alt = "Kirby Logo"/></Link></div>
-          <form onSubmit = {(e) => {handleSearchClick(e)}} className = {`${styles["search-bar-form"]} ${styles["desktop-icon-container"]}`}>
-            <input type = "text" placeholder = "Search Products..."/>
-            <button type = "submit"><i className="fa-solid fa-magnifying-glass"></i></button>
-          </form>
-          <div className = {styles["profile-cart-container"]}>
-            <Link to="/" className = {styles["desktop-icon-container"]}><i className="fa-2x fa-solid fa-user"></i><span>Sign In/Register</span></Link>
-            {(cartItems.length === 0) ?
-              <Link to="/cart"><img className = {styles["cart-icon"]} src = {cartIcon}/><span>Cart</span></Link> :
-              <Link to="/cart">
-                <div className = {styles["cart-icon-container"]}>
-                  <img className = {styles["cart-icon"]} src = {kirbyCartIcon} />
-                  {cartItemQuantity > 99 ? 
-                    <p className = {`${styles["cart-quantity"]} ${styles["three-digits"]}`}>99+</p> :
-                    <p className = {`${styles["cart-quantity"]} ${styles["less-than-two-digits"]}`}>{cartItemQuantity}</p>
-                  }
-                </div>
-                <span>Cart</span>
-              </Link>
-            }
-          </div>
-        </div>
-        <SubHeader />
-      </header>   
-    )
-  }
+  const headerClasses = `${styles.header} ${atTop ? styles.opaque : ''}`;
 
   return (
-    <header className = {styles.header}>
+    <header className = {headerClasses}>
       <div className = {styles["container-header"]}>
         {/* mobile hamburger bar */}
         <div className = {styles["mobile-icon-container"]}><i className="fa-2x fa-solid fa-bars"></i></div>        
