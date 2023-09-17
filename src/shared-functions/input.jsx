46c020f156c1e1eb3603425208bc70d14a3139e9
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-17 15:25:32 -0700

    add functions to prevent input of negative quantity

---
+++ src/shared-functions/input.jsx
@@ -0,0 +1,16 @@
+const preventMinus = (e) => {
+  if (e.code === 'Minus') {
+      e.preventDefault();
+  }
+};
+
+const preventPasteNegative = (e) => {
+  const clipboardData = e.clipboardData || window.clipboardData;
+  const pastedData = parseFloat(clipboardData.getData('text'));
+
+  if (pastedData < 0) {
+      e.preventDefault();
+  }
+};
+
+export {preventMinus, preventPasteNegative};
