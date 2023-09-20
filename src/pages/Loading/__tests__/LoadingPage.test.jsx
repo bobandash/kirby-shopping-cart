ea78ddbd56e99ebd7274f26f36e33cd5bcaaddf1
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-19 21:35:02 -0700

    add loading screen tests

---
+++ src/pages/Loading/__tests__/LoadingPage.test.jsx
@@ -0,0 +1,13 @@
+import {describe, it, expect} from 'vitest'
+import { render } from "@testing-library/react";
+import LoadingScreen from '../LoadingPage';
+
+// setups props
+describe('Loading Screen', () => {
+  it("matches snapshot", () => {
+    const {container} = render(
+      <LoadingScreen />
+    );
+    expect(container).toMatchSnapshot();
+  })
+})
