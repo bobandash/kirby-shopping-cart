91ad3d59e9da225bc9c462ed2b6ea05a0448e353
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-19 21:27:53 -0700

    change toFixed to convertCurrencyFormat shared function

--- src/pages/Cart/CartItemsFooter.jsx
+++ src/pages/Cart/CartItemsFooter.jsx
@@ -1,5 +1,6 @@
 import styles from "./Cart.module.css"
 import PropTypes from 'prop-types'
+import { convertCurrencyFormat } from "../../utils/currency"
 
 //TO-DO: add checkout button functionality
 // add error in case customer enters negative quantity
@@ -7,7 +8,7 @@ function CartItemsFooter({totalPrice}){
   return (
     <section className = {styles["cart-item-footer"]}>
       <div className = {styles["footer-contents"]}>
-        <h1>Total: ${totalPrice.toFixed(2)}</h1>
+        <h1>Total: ${convertCurrencyFormat(totalPrice)}</h1>
         <button>Checkout<i className="fa-solid fa-arrow-right"></i></button>
       </div>
     </section>
