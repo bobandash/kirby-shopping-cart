4ccf966f2870d82ad89618d92f120971544655df
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-20 20:39:01 -0700

    remove unused imports

--- src/pages/Cart/__tests__/CartItem.test.jsx
+++ src/pages/Cart/__tests__/CartItem.test.jsx
@@ -1,7 +1,7 @@
 
 import CartItem from "../CartItem"
-import {describe, it, expect, beforeEach, vi} from 'vitest'
-import { render, screen, act} from "@testing-library/react";
+import {describe, it, expect, vi} from 'vitest'
+import { render, screen} from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 
 describe("Cart Item", () => {
