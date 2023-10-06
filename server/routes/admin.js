f8dd0538afb02b4f1ef53c32d5dcd5d9fcf58987
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-06 16:35:05 -0700

    add route to product instance

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../controller/productController')
+const productInstanceController = require('../controller/productInstanceController');
 
-/* GET home page. */
-
+/* FOR CATEGORY OPERATIONS */
 router.get('/api/products/:category', productController.products_list);
+
+/* FOR INDIVIDUAL PRODUCT OPERATIONS */
+router.get('/api/products/id/:id', productInstanceController.product_information)
 
 module.exports = router;
